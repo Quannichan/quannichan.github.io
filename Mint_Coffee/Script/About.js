19e13a22eca553af91deaf0746307d1486dcc96f
Trần Minh Quân <tranminhquan130804@gmail.com>
2023-04-06 01:53:03 +0700

    update-demo1

---
+++ Mint_Coffee/Script/About.js
@@ -0,0 +1,14 @@
+ScrollReveal({
+    distance:'150px',
+    duration: 2500,
+    delay:300
+});
+ScrollReveal().reveal('.header',{delay:300, origin:'top',reset:true});
+ScrollReveal().reveal('.tt1',{delay:300, origin:'left',reset:false});
+ScrollReveal().reveal('.text01',{delay:450, origin:'left',reset:false});
+ScrollReveal().reveal('.img01',{delay:300, origin:'bottom',reset:false});
+ScrollReveal().reveal('.line',{delay:300, origin:'top',reset:false});
+ScrollReveal().reveal('.img02',{delay:300, origin:'top',reset:false});
+ScrollReveal().reveal('.tt2',{delay:300, origin:'left',reset:false});
+ScrollReveal().reveal('.text02',{delay:300, origin:'bottom',reset:false});
+
---
+++ Mint_Coffee/Script/index.js
@@ -0,0 +1,10 @@
+ScrollReveal({
+    distance:'150px',
+    duration: 2500,
+    delay:300
+});
+ScrollReveal().reveal('.container',{delay:300, origin:'top',reset:false});
+ScrollReveal().reveal('.image1',{delay:300, origin:'bottom',reset:false});
+ScrollReveal().reveal('.image2',{delay:300, origin:'top',reset:false});
+ScrollReveal().reveal('.image3',{delay:300, origin:'left',reset:false});
+ScrollReveal().reveal('.texxt',{delay:300, origin:'bottom',reset:false});
